4e81f1fa503dd495d04115b97614b9e2254d71d0
rjmandloi <rjmandloi@isystango.com>
2022-05-25 17:49:32 +0530

    Initialize app Create your text

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar title="Create yout text" aboutText="About us" />
+    </>
+  );
+}
+
+export default App;
